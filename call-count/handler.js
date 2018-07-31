b0e90499881d951ecb7d0410561970c10dd514ec
Patricio Diaz <padiazg@gmail.com>
2018-07-31 11:57:03 -0400

    Time zone management is added. Metrics without variation are not included in the output.

--- call-count/handler.js
+++ call-count/handler.js
@@ -8,25 +8,33 @@ const prometheusUrl = process.env.prometheus || 'http://prometheus:9090';
 // const prometheusUrl = 'http://10.22.1.80:9090';
 const uri = `${prometheusUrl}/api/v1/query_range`;
 
+// set de default timezone
+if (process.env.timezone) {
+    moment.tz.setDefault(process.env.timezone);
+}
+
 const outputTable = data => {
     let text = '';
     for (const {metric, values} of data.result) {
         const table = new AsciiTable('Functions');
-
-        table.fromJSON({
-            heading: ['','Day/Hour', 'Accum. Calls', 'Calls'],
-            rows: values.map((v,i,x) => {
-                return [
-                    i+1,    // index
-                    moment(v[0], 'X').format("YYYY-MM-DD HH:MM"),   // Day/Hour
-                    v[1],                                           // Accummulated calls
-                    i>0 ? parseInt(v[1])-parseInt(x[i-1][1]) : ''   // Calls in this step
-                ] // return ...
-            }) // rows: ...
-        }); // fromJSON ...
-
         const sum = parseInt(values[values.length-1][1]) - parseInt(values[0][1]);
-        text += `\n${metric.function_name}/${metric.code} sum=${sum}\n${table.toString()}`
+
+        // only include if there is a value in the range
+        if (sum) {
+            table.fromJSON({
+                heading: ['','Day/Hour', 'Accum. Calls', 'Calls'],
+                rows: values.map((v,i,x) => {
+                    return [
+                        i+1,    // index
+                        moment(v[0], 'X').format("YYYY-MM-DD HH:MM"),   // Day/Hour
+                        v[1],                                           // Accummulated calls
+                        i>0 ? parseInt(v[1])-parseInt(x[i-1][1]) : ''   // Calls in this step
+                    ] // return ...
+                }) // rows: ...
+            }); // fromJSON ...
+
+            text += `\n${metric.function_name}/${metric.code} sum=${sum}\n${table.toString()}`
+        } // if (sum) ...
     } // for ...
     return text;
 } // outputTable ...
@@ -41,10 +49,12 @@ module.exports = (context, callback) => {
     let format =  incomming.format ? incomming.format.toLowerCase() : 'table';
     format = /^(table|json)$/i.test(format) ? format : 'table';
 
+    // Prometheus handles only UTC datetime so we use momment-timezone to
+    // convert the provided dates to the correct value.
     const qs = {
         query: 'gateway_function_invocation_total',
-        start: moment(incomming.start, "YYYY-MM-DD").format(),
-        end: moment(incomming.end, "YYYY-MM-DD").format(),
+        start: moment(incomming.start, "YYYY-MM-DD").utc().format(),
+        end: moment(incomming.end, "YYYY-MM-DD").utc().format(),
         step: incomming.step || '1h'
     }
 
